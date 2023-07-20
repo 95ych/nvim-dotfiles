13d658e37aab2a377400fc525f90cced96856c23
95ych <cyksagar@protonmail.com>
2023-07-20 16:18:41 +0530

    test config

--- init.lua
+++ init.lua
@@ -467,11 +467,11 @@ local servers = {
   gopls = {},
   pyright = {},
   rust_analyzer = {},
-  jdtls = {
-    java = {
-      signatureHelp = { enabled = true }
-    }
-  },
+  -- jdtls = {
+  --   java = {
+  --     signatureHelp = { enabled = true }
+  --   }
+  -- },
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
 
@@ -669,5 +669,65 @@ vim.g.vimtex_view_method = "zathura"
 vim.o.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.cmd("let g:UltiSnipsSnippetDirectories = ['/home/cyk/.config/nvim/latex-snippets']")
+
+local jconfig = {
+  -- The command that starts the language server
+  -- See: https://github.com/eclipse/eclipse.jdt.ls#running-from-the-command-line
+  cmd = {
+
+    "java", -- or '/path/to/java17_or_newer/bin/java'
+    -- depends on if `java` is in your $PATH env variable and if it points to the right version.
+
+    "-javaagent:/home/jake/.local/share/java/lombok.jar",
+    -- '-Xbootclasspath/a:/home/jake/.local/share/java/lombok.jar',
+    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+    "-Dosgi.bundles.defaultStartLevel=4",
+    "-Declipse.product=org.eclipse.jdt.ls.core.product",
+    -- '-noverify',
+    "-Xms1g",
+    "--add-modules=ALL-SYSTEM",
+    "--add-opens",
+    "java.base/java.util=ALL-UNNAMED",
+    "--add-opens",
+    "java.base/java.lang=ALL-UNNAMED",
+    "-jar",
+    vim.fn.glob(
+      "/Users/sagar/.local/share/nvim/lsp_servers/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
+    -- Must point to the                                                     Change this to
+    -- eclipse.jdt.ls installation                                           the actual version
+
+    "-configuration",
+    "/Users/sagar/.local/share/nvim/lsp_servers/jdtls/config_mac",
+    -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
+    -- Must point to the                      Change to one of `linux`, `win` or `mac`
+    -- eclipse.jdt.ls installation            Depending on your system.
+
+    -- See `data directory configuration` section in the README
+  },
+
+  -- This is the default if not provided, you can remove it. Or adjust as needed.
+  -- One dedicated LSP server & client will be started per unique root_dir
+  root_dir = require("jdtls.setup").find_root({ ".git",
+    "mvnw", "gradlew" }),
+
+  -- Here you can configure eclipse.jdt.ls specific settings
+  -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
+  -- for a list of options
+  settings = {
+    java = {
+      signatureHelp = { enabled = true }
+    },
+  },
+  handlers = {
+    ["language/status"] = function(_, result)
+      -- print(result)
+    end,
+    ["$/progress"] = function(_, result, ctx)
+      -- disable progress updates.
+    end,
+  },
+}
+-- require('jdtls').start_or_attach(jconfig)
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
