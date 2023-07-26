25e6945fc8c086ed5fe2c3fb3203b2c74d43d385
95ych <cyksagar@protonmail.com>
2023-07-26 14:59:59 +0530

    java sign update

--- lua/custom/plugins/java.lua
+++ lua/custom/plugins/java.lua
@@ -3,6 +3,6 @@ return {
 	'puremourning/vimspector',
 	'williamboman/nvim-lsp-installer',
 	'mfussenegger/nvim-jdtls',
-	{ 'artur-shaik/jc.nvim', opts = {} },
+	-- { 'artur-shaik/jc.nvim', opts = {} },
 
 }
