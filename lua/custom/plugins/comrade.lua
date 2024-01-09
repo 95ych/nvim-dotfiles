9fc02a39388f849d6787fecbe80d7fb0d40f334a
C Y K Sagar <cyksagar@protonmail.com>
2024-01-09 23:04:42 +0530

    remove comrade

--- lua/custom/plugins/comrade.lua
+++ lua/custom/plugins/comrade.lua
@@ -1,13 +1,13 @@
 return {
-	'Shougo/deoplete.nvim',
-	run = ':UpdateRemotePlugins',
-	requires = {
-		'roxma/nvim-yarp',
-		'roxma/vim-hug-neovim-rpc',
-		run = 'pip install -r requirements.txt'
-	},
-	config = function()
-		require('user.plugin_options.deoplete')
-	end,
-	'beeender/Comrade'
+	-- 'Shougo/deoplete.nvim',
+	-- run = ':UpdateRemotePlugins',
+	-- requires = {
+	-- 	'roxma/nvim-yarp',
+	-- 	'roxma/vim-hug-neovim-rpc',
+	-- 	run = 'pip install -r requirements.txt'
+	-- },
+	-- config = function()
+	-- 	require('user.plugin_options.deoplete')
+	-- end,
+	-- 'beeender/Comrade'
 }
