19ff626e1219086193c29e8852f47235f0863281
C Y K Sagar <cyksagar@protonmail.com>
2024-02-16 12:35:40 +0530

    shorcut for SessionLoad

--- lua/custom/plugins/session.lua
+++ lua/custom/plugins/session.lua
@@ -3,5 +3,6 @@ return {
 	cmd = { 'SessionSave', 'SessionDelete', 'SessionLoad' },
 	opts = {
 		auto_save_on_exit = true
-	}
+	},
+	vim.keymap.set("n", "<leader>sl", "<cmd>SessionLoad<CR>", { noremap = true, silent = true }),
 }
