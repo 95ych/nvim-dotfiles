903034a23cb99e581193b68de040ae0ca6f83c4b
C Y K Sagar <cyksagar@protonmail.com>
2024-02-16 12:07:16 +0530

    bind shortcut to TermToggle instead of TermNew

--- lua/custom/plugins/floatterm.lua
+++ lua/custom/plugins/floatterm.lua
@@ -1,6 +1,6 @@
 return {
 	'voldikss/vim-floaterm',
-	vim.keymap.set("n", "<leader>tt", "<cmd>FloatermNew<CR>", { noremap = true, silent = true }),
+	vim.keymap.set("n", "<leader>tt", "<cmd>FloatermToggle<CR>", { noremap = true, silent = true }),
 	vim.api.nvim_set_keymap('t', '<Leader><ESC>', '<C-\\><C-n>', { noremap = true })
 
 }
