bba1142704af245047f2d87aca5a8d358c2e93d8
C Y K Sagar <cyksagar@protonmail.com>
2024-02-20 14:23:15 +0530

    cleanup

--- lua/custom/plugins/java.lua
+++ lua/custom/plugins/java.lua
@@ -8,15 +8,6 @@ return {
 		'MunifTanjim/nui.nvim',
 		'neovim/nvim-lspconfig',
 		'mfussenegger/nvim-dap',
-		{
-			'williamboman/mason.nvim',
-			opts = {
-				registries = {
-					'github:nvim-java/mason-registry',
-					'github:mason-org/mason-registry',
-				},
-			},
-		}
 	},
 	config = function()
 		require('java').setup(
